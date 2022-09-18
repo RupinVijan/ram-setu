6e14ce5951e248c0496f7d0514713efd66b35824
anshd258 <anshd258@gmail.com>
2022-09-19 02:06:35 +0530

    made it responsive for most of the phones

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -3,6 +3,11 @@ import "../../assets/css/contact.css"
 const Contact = () => {
     return (
         <div className="background d-flex justify-content-center">
+        <div  className="image">
+        
+         {/* <img  className= "imag" src={require('../../assets/images/Login_Background.png') }/> */}
+        </div>
+
             {/* <img src={require('../../assets/images/LableBox.png')}/> */}
         </div>
     );
