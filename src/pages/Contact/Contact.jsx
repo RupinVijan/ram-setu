bd4110064b273b1144b670da02256a4566d958bb
anshd258 <anshd258@gmail.com>
2022-09-19 15:31:02 +0530

    added input fields  and ok button on modal

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -3,12 +3,12 @@ import "../../assets/css/contact.css"
 const Contact = () => {
     return (
         <div className="background d-flex justify-content-center">
-        <div  className="image">
-        
-         {/* <img  className= "imag" src={require('../../assets/images/Login_Background.png') }/> */}
-        </div>
+      
 
-            {/* <img src={require('../../assets/images/LableBox.png')}/> */}
+            <img src={require('../../assets/images/LableBox.png')} className ="input-box"/>
+            <input type='text' className='nameinput' placeholder='name'/>
+            <input type='text' className='noinput' placeholder='pone.no'/>
+            <button type='submit' className='button1'>OK</button> 
         </div>
     );
 };
