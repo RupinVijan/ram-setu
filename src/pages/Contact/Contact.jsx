e9e1642fe82c7bba918f243645bcb037662ff77e
LordFLader <devyansh1kalra@gmail.com>
2022-09-21 02:27:54 +0530

    added localStorage functionality

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,14 +1,46 @@
-import React from 'react';
+import React, { useRef, useEffect, useState } from "react";
 import "../../assets/css/contact.css"
 const Contact = () => {
+    const [name,setName]= useState();
+    const [number,setNumber]= useState();
+    const save=()=>{
+        window.localStorage.setItem("name",name);
+        window.localStorage.setItem("number",number);
+        console.log("item saved",name,number);
+    }
+    useEffect( () =>{
+        
+        const data1=window.localStorage.getItem("name");
+        const data2=window.localStorage.getItem("number");
+        
+        setName(data1);
+        setNumber(data2);
+    } ,[])
+    // useEffect( () =>{
+        
+    //     window.localStorage.setItem("name",name);
+    //     window.localStorage.setItem("number",number);
+    //     console.log(name,number);
+    // } ,[name,number])
+    
     return (
         <div className="background d-flex justify-content-center">
       
 
             <img src={require('../../assets/images/LableBox.png')} className ="input-box"/>
-            <input type='text' className='nameinput' placeholder='name'/>
-            <input type='text' className='noinput' placeholder='pone.no'/>
-            <button type='submit' className='button1'>OK</button> 
+            <label className='labelName d-flex'></label>
+            
+            <input type='text' className='nameinput' placeholder='Name' value={name} 
+            onChange={(e) => {setName(e.target.value)}} 
+            />
+            
+            <input type='text' className='noinput' placeholder='Phone number' value={number} 
+            onChange={(e) => {setNumber(e.target.value)}}
+            />
+
+            <label className='labelNumber d-flex justify-content-center'></label>
+            <button type='submit' className='button1' onClick={save} >OK</button> 
+
         </div>
     );
 };
