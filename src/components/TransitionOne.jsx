173da5e5e4e3d63ce11caa6bb62849d13a7b9964
Harpreet Singh <harpreets1602@gmail.com>
2022-09-28 02:21:34 +0530

    Added the video in the home page

--- src/components/TransitionOne.jsx
+++ src/components/TransitionOne.jsx
@@ -1,10 +1,16 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import "../assets/css/TransitionOne.css";
 import image from "../assets/images/image.png";
 import hanuman from "../assets/images/hanuman.png";
 // import audIntro from "../assets/audio/audIntro.mp3";
+import homeVideo from "../assets/video/FInal_Render.mp4";
+import silentVideo from "../assets/video/silent_video.mp4";
+import homeAudio from "../assets/audio/FInal_Render.mp3";
+import initialImage from "../assets/images/initial-image.png";
 const TransitionOne = () => {
   let hoverCheck = 0;
+  const ref = useRef(null);
+
   // const audio = new Audio(audIntro);
   // const start = () => {
   //   audio.play();
@@ -21,10 +27,20 @@ const TransitionOne = () => {
     }
   }
 
+  function playVideo(e){
+    console.log(e.target);
+    e.target.style.display = "none";
+    const videoPlay = ref.current;
+    videoPlay.play();
+
+
+
+  }
+
   return (
     <div>
 
-      <div
+      {/* <div
         className="bgBack"
         onMouseEnter={() => {
           callHover();
@@ -35,7 +51,16 @@ const TransitionOne = () => {
         </div>
         <img src={image} className="image" alt="water" />
       
-      </div>
+      </div> */}
+      <img className="initialImage" src={initialImage} onClick={(e)=>{
+        playVideo(e);
+      }}/>
+      <video  autoPlay className="homeVideo" id="myVideo"  ref={ref} >
+        <source src={homeVideo} type="video/mp4"/>
+      </video>
+
+     {/* <iframe src={homeVideo} allow='autoPlay' className="homeVideo"></iframe> */}
+ 
     </div>
   );
 };
