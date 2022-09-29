9669616f361243e77dca0d8558224149165cdfe3
Daksh2356 <dakshmakhija@gmail.com>
2022-09-29 17:57:39 +0530

    Ram_Sita page added

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import { Routes, Route } from "react-router-dom";
 import Setu from "./components/Setu";
 import Contact from "./pages/Contact/Contact";
 import Mask from "./pages/Mask/Mask";
+import Ram_Sita from "./pages/Ram_Sita/Ram_Sita";
+
 import React, { useEffect, useState } from "react";
 function App() {
-  const[trigger,setTrigger]=useState(false);
-  const handleTrigger = () =>{
+  const [trigger, setTrigger] = useState(false);
+  const handleTrigger = () => {
     setTrigger(true);
-    }
-  
+  };
+
   return (
     <div className="App">
       <Routes>
@@ -18,11 +20,10 @@ function App() {
         <Route path="/setu" element={<Setu />} />
         <Route path="/mask" element={<Mask />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/ram-sita" element={<Ram_Sita />} />
       </Routes>
-    
     </div>
-    
   );
-  }
+}
 
 export default App;
---
+++ src/pages/Ram_Sita/Ram_Sita.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "../../assets/css/Ram_Sita.css";
+import video from "../../assets/video/Final_Render_2.mp4";
+
+const Ram_Sita = () => {
+  return (
+    <div className="topContainer">
+      <video
+        autoPlay
+        loop
+        muted
+        className="RamSitaGIFContainer"
+        src={video}
+      ></video>
+    </div>
+  );
+};
+
+export default Ram_Sita;
