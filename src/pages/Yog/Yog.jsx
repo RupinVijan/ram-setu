5339160c978e71b0d1c5823d239b4af15cf30f6c
LordFLader <devyansh1kalra@gmail.com>
2022-10-21 20:35:51 +0530

    minor fixes and screen width functionality

--- src/pages/Yog/Yog.jsx
+++ src/pages/Yog/Yog.jsx
@@ -1,42 +1,35 @@
-import classes from "./Yog.module.css"
+import classes from "./Yog.module.css";
 
-
-import React from "react"
+import React from "react";
 import { useNavigate } from "react-router-dom";
 
 const Yog = () => {
-    // const v1 = "https://res.cloudinary.com/dde6glimb/video/upload/v1665924712/1_1_di76xj.mp4";
-    // const v2 = "https://res.cloudinary.com/dde6glimb/video/upload/v1665924729/2_1_ancwjy.mp4";
-    // const v3 ="https://res.cloudinary.com/dde6glimb/video/upload/v1665924742/3_1_wpt0jo.mp4";
-    const v = "https://res.cloudinary.com/dde6glimb/video/upload/v1665915568/Yoga_Video_1_o9ddgc.mp4"
+    function getWindowDimensions() {
+        const { innerWidth: width, innerHeight: height } = window;
+        return {
+          width,
+          height
+        };
+    }
+    let screenWidth = getWindowDimensions().width;
 
-    // let alreadyScrolled = false
+  const v =
+  screenWidth>600?
+    "https://res.cloudinary.com/dde6glimb/video/upload/v1665915568/Yoga_Video_1_o9ddgc.mp4":
+    "https://res.cloudinary.com/dde6glimb/video/upload/v1666363192/M-3_hupvky.mp4";
 
-    // const [vidIndex, setVidIndex] = useState(1);
-    // const [vidVar, setVidVar] = useState(v1) ;
+  const navigate = useNavigate();
+  return (
+    <div className={classes.container}>
+      <video
+        src={v}
+        playsInline
+        className={classes.vidStyles}
+        autoPlay
+        onEnded={() => navigate("/yogaToken")}
+      />
+    </div>
+  );
+};
 
-
-    // const changeVid = () => {
-    //     const toSetIndex = vidIndex < 3 ?  (vidIndex +1) : 3;
-    //     toSetIndex ===1 ?setVidVar(v1): toSetIndex===2? setVidVar(v2): toSetIndex===3? setVidVar(v3):setVidVar(v3);
-    //     setVidIndex(toSetIndex);
-    // }
-
-    // window.onscroll = ()=>{
-    //     if(!alreadyScrolled){
-    //        alreadyScrolled = true
-    //         setTimeout(()=>{
-    //             alreadyScrolled = false
-    //         },1500)
-    //         changeVid()
-    //     }
-    // }
-    const navigate = useNavigate();
-    return (
-        <div className={classes.container} >
-            <video src={v} className={classes.vidStyles} autoPlay onEnded={()=>navigate("/yogaToken")}/>
-        </div>
-    )
-}
-
-export default Yog
+export default Yog;
