43428fa510408ff2863a3790f4dea463564a8ec7
anshd258 <anshd258@gmail.com>
2022-10-21 20:19:46 +0530

    comm

--- src/App.js
+++ src/App.js
@@ -5,12 +5,13 @@ import Setu from "./components/Setu";
 import Contact from "./pages/Contact/Contact";
 import Mask from "./pages/Mask/Mask";
 import RamSita from "./pages/Ram_Sita/Ram_Sita";
-import Bg_Video from "./assets/video/Waves.mp4";
+
 import Satyug from "./pages/Satyug";
 import Yog from "./pages/Yog/Yog";
 import React from "react";
 import TestModal from "./pages/TestModal/TestModal";
 function App() {
+  const Bg_Video = "https://res.cloudinary.com/dde6glimb/video/upload/v1665922766/Waves_vsucxi.mp4";
   return (
     <div className="App">
       {window.location.href.includes("ram-setu") ? (
